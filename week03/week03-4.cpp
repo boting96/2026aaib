48a7e3cb6bec37e9e753b84eca1e450e0bc9be87
boting96 <14750704@me.mcu.edu.tw>
2026-03-11 11:59:13 +0800

    week03

--- week03/week03-4.cpp
+++ week03/week03-4.cpp
@@ -1,14 +1,14 @@
-//week03-3b.cpp SOIT106_BASE_010
+//week03-4.cpp SOIT106_BASE_005
+
 #include <stdio.h>
 
 int main()
 {
+	int N;
+	scanf("%d", &N);
 	int ans = 0;
-	for (int i=0; i<10; i++){
-		int b;
-		scanf("%d", &b);
-		if (b%3==0) ans++;
+	for (int i = 1; i<=N; i++){
+		if (N%i==0) ans++;
 	}
-	printf("%d\n", ans);
-
+	printf("%d\n",ans);
 }
